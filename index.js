b6d952d372159b2bde9c5a6fb11d65b3edf8595b
bkhabib <habibur.vivasoft@bkash.com>
2023-09-18 22:16:11 +0600

    Add Login Page

---
+++ index.js
@@ -0,0 +1,67 @@
+/*
+  1. call stack
+  2. execution context
+    Parts:
+      a. memory component or variable envrionment
+      b. code component or thread of execution
+    Phases:
+      a. creation phase or memory creation phase
+      b. execution phase or code execution phase
+  3. Hoisting
+  4. Global Execution context
+  5. Local Execution context
+  6. Reference Error: undefined is not a function
+  7. function
+    a. function statement or function declaration
+    b. function expression
+    c. IIFE
+    d. anonymous function
+    e. arrow function
+    f. first class function
+    g. callback function
+  8. object, generator function, constructor function, syntactical sugar, class, this
+  9. object destructuring
+  10. extends (inheritance), super
+
+  class, constructor function, generator function, object
+
+  11. scope, 
+  12. scope chain, 
+  13. lexical environment: memeory component + the lexical envrionment of it's parent.
+  14. closure: funcion along with it's lexical environment forms a closure
+
+  15. block => { }
+  16. Memory Leaking problem
+  17. let, const
+  18. immutable variable
+  19. temporary dead zone => সাময়িক মরে যাওয়া অবস্থা => সময়/time
+  20. variable shadowing
+  21. script scope, block scope, global scope
+  22. hoisting in let, const
+  23. browser
+  24. javascript engine
+  25. browser features
+  26. web api
+  27. event loop
+  28. callback queue
+  29. priority queue
+*/
+
+/*
+  1. installed node js
+  2. installed create-react-app with npm (npm i -g create-react-app)
+  3. created a project with create-react-app
+  4. clean the project
+  5. explanation of the project
+*/
+
+/*
+  1. var (variable declaration)
+  2. if ... else
+  3. for, while, do--while loop
+  4. function statement/function declaration
+  5. class 
+  6. object
+
+  => React
+*/
